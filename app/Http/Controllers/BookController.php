d45f2f4b7dd645fa5500f8abe1e429132fd23b7e
murakyra <yafiariella8@gmail.com>
2025-12-01 08:16:00 +0700

    Fix image upload and view

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -57,7 +57,7 @@ class BookController extends Controller
 
         if ($request->hasFile('cover_image')) {
             $imageName = time().'.'.$request->cover_image->extension();
-            $request->cover_image->storeAs('public/covers', $imageName);
+            $request->cover_image->storeAs('covers', $imageName, 'public');
             $book->cover_image = 'covers/'.$imageName;
         }
 
